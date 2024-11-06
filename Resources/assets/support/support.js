6f65013a01ed4d88f7fa880f8fb2a9a49bceaf2c
kingstar <admin@baks.dev>
2024-11-06 14:53:11 +0300

    Baks Development

--- Resources/assets/support/support.js
+++ Resources/assets/support/support.js
@@ -23,6 +23,6 @@
 var btn = document.querySelector(".offcanvas-link");
 
 btn.addEventListener("click",
-    document.querySelector(".box-hidden").scrollIntoView({block: "center"})
+    document.querySelector(".box-hidden")?.scrollIntoView({block: "center"})
 );
 
