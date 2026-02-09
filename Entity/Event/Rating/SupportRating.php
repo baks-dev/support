80243ffbbddd1863ae3ddeb3086bb46a77e90ccf
MichelAngelo <baksdevelopmen@gmail.com>
2026-02-09 14:02:27 +0300

    BaksDevelopment

--- Entity/Event/Rating/SupportRating.php
+++ Entity/Event/Rating/SupportRating.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -90,7 +90,7 @@ class SupportRating extends EntityReadonly
     /** @var SupportRatingInterface $dto */
     public function setEntity($dto): mixed
     {
-        if($dto instanceof SupportRatingInterface)
+        if($dto instanceof SupportRatingInterface || $dto instanceof self)
         {
             if(true === empty($dto->getValue()))
             {
